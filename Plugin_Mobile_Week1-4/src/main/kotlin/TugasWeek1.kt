b7e28f567eda6c9942cecfd74ec8bdf60887111c
Avriansyah Bahtiar <92713979+Avrians@users.noreply.github.com>
2021-12-14 10:58:20 +0700

    Versi 1

---
+++ Plugin_Mobile_Week1-4/src/main/kotlin/TugasWeek1.kt
@@ -0,0 +1,12 @@
+fun main() {
+    val biodata = """
+        Nama    : Avriansyah Bahtiar
+        NIM     : 21090024
+        TTL     : Brebes, 10 April 2000
+        Kelas   : 1A
+        Prodi   : DIV Teknik Informatika
+    """.trimIndent()
+
+    println(biodata)
+    println("\nPanjang Karakternya = "+biodata.length)
+}
